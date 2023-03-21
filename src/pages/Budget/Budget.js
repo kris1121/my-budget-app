d2c133d4214df2777b8a12627fc7a3fdff740365
Krzysztof Matyga <krzysztof.matyga@noinputsignal.com>
2023-03-21 20:08:39 +0100

    basic modal and routing added

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useMemo } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 
 import { fetchBudgetAction, fetchBudgetedCategoriesAction } from 'data/actions/budget.actions'
 import { fetchAllCategoriesAction } from 'data/actions/common.actions';
 import { Grid } from './Budget.css';
-import { Loadingindicator } from 'components';
+import { Loadingindicator, Modal, Button } from 'components';
 import BudgetCategoryList from './components/BudgetCategoryList';
 import BudgetTransactionList from './components/BudgetTransactionList/BudgetTransactionList';
 
@@ -20,18 +21,30 @@ const Budget = () => {
     useEffect(() => {
         dispatch(fetchBudgetAction(1));
         dispatch(fetchBudgetedCategoriesAction(1));
-      dispatch(fetchAllCategoriesAction());
+        dispatch(fetchAllCategoriesAction());
     }, [dispatch])
 
   return (
-    <Grid>
-     <section>
-        {isLoaded ? <BudgetCategoryList /> : <Loadingindicator />}
-      </section>
-     <section>
-        {isLoaded ? <BudgetTransactionList /> : <Loadingindicator />}
-      </section>
-    </Grid>
+    <>
+      <Grid>
+      <section>
+          {isLoaded ? <BudgetCategoryList /> : <Loadingindicator />}
+        </section>
+      <section>
+          {isLoaded ? (
+            <>
+              <Button to={"/budget/transaction/new"}>Add new transaction</Button>
+              <BudgetTransactionList />
+            </>
+         ): <Loadingindicator />}
+        </section>
+      </Grid>
+      <Switch>
+        <Route path="/budget/transaction/new">
+          <Modal>Modal</Modal>
+        </Route>
+      </Switch>
+    </>
   )
 }
 
